3c0013f16f9c90c81671ef211e86ef5c4af2a4a5
Ashim Rudra Paul <ashimrudrapaul23@gmail.com>
2022-09-23 02:20:14 +0600

    added modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 import './Modal.css'
 
-const Modal = () => {
+const Modal = ({ modalData, setModalData }) => {
+
     return (
-        <div>
-            
-        </div>
+        <section>
+            <input type="checkbox" id="my-modal-3" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box relative">
+                    <label htmlFor="my-modal-3" className="btn btn-sm btn-circle absolute right-2 top-2"
+                        onClick={() => setModalData(null)}
+                    >
+                        ✕
+                    </label>
+                    <h3 className="text-lg font-bold">{modalData.name}</h3>
+                    <p className="py-4">You've been selected for a chance to get one year of subscription to use Wikipedia for free!</p>
+                </div>
+            </div>
+        </section>
     );
 };
 
--- src/components/SingelGan/SingelGan.jsx
+++ src/components/SingelGan/SingelGan.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
+import Modal from '../Modal/Modal';
 import './SingelGan.css'
+import { useState } from 'react';
 
 const SingelGan = (props) => {
+    const [modalData, setModalData] = useState({})
+    const { Incares, gun } = props
     const { img, name, bullet, action, category, price } = props.gun;
-    const { Incares } = props
     return (
         <div className="card w-80 md:w-96 lg:w-full bg-base-100 shadow-xl p-4">
             <figure><img src={img} alt="Guns" className='gunImg' /></figure>
             <div className="card-body">
                 <h2 className="card-title">
-                    <h5 style={{ fontSize: '0.8rem' }}>{name}</h5>
-                    <div className="badge badge-secondary">
-                        <small style={{ fontSize: '0.56rem' }}>{action}</small>
-                    </div>
+                    <h5 style={{ fontSize: '1.4rem' }}>{name}</h5>
                 </h2>
+                <div className="badge badge-secondary">
+                    <small style={{ fontSize: '1rem' }}>{action}</small>
+                </div>
                 <h3 className='text-left'>Price : {price}</h3>
                 <div className='flex justify-start'>
                     <button className="btn btn-sm btn-accent mr-3" onClick={() => Incares()}>Add To Cart</button>
-                    <button className="btn btn-sm btn-primary mx-4">Details</button>
+                    {/* <button className="btn btn-sm btn-primary mx-4">Details</button> */}
+                    <label onClick={() => { setModalData(gun) }} htmlFor="my-modal-3" className="btn btn-sm btn-primary mx-4 modal-button">
+                        Details
+                    </label>
                 </div>
-                <div className="card-actions justify-start" style={{ fontSize: '0.8rem' }}>
+                <div className="card-actions justify-start" style={{ fontSize: '1rem', marginTop: '0.7rem' }}>
                     <span>Category : <div className="badge badge-outline">{category}</div> </span>
                     <span>Bollet: <div className="badge badge-outline">{bullet}</div></span>
                 </div>
             </div>
+            {
+                modalData && (
+                    <Modal modalData={modalData} setModalData={setModalData}></Modal>
+                )
+            }
         </div>
     )
 };
